6d8aadd4320651edcee05c5e5ede7c86cf6f53c3
Rojo-123 <cristiantorresraposo@email.com>
2024-12-21 10:58:43 -0500

    fix: Targetfriend

--- app/components/targetfriend/Targetfriend.tsx
+++ app/components/targetfriend/Targetfriend.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Button from "../buttons/Buttons";
 
-interface TargetProps{
-  name: string,
-  photo: string,
+interface TargetProps {
+  name: string;
+  photo: string;
 }
 
-const TargetFriend: React.FC<TargetProps> = ({name, photo}) => {
+const TargetFriend: React.FC<TargetProps> = ({ name, photo }) => {
   return (
     <>
-        <div className="w-[15em] h-[25em] bg-[#fff1] rounded-lg p-2">
-          <img src={photo} alt="" className="rounded-lg w-56 h-52" />
-          <h2 className="m-2 font-bold">{name}</h2>
-          {/*Falta lo de amigos en comun*/}
+      <div className="w-[15em] h-[25em] bg-[#fff1] rounded-lg p-2">
+        <img src={photo} alt="" className="rounded-lg w-56 h-52" />
+        <h2 className="m-2 font-bold absolute max-w-52">{name}</h2>
+        {/*Falta lo de amigos en comun*/}
+        <div className="mt-16">
           <Button
             className="font-bold mt-3 w-full bg-[#07f6] p-4 pt-2 pb-2 rounded-lg hover:bg-[#cff6]"
             text="Añadir Amigo"
@@ -22,7 +23,7 @@ const TargetFriend: React.FC<TargetProps> = ({name, photo}) => {
             text="Suprimir"
           />
         </div>
-      
+      </div>
     </>
   );
 };
